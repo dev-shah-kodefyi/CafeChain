8e4e19d366c765eb10f338b5ecc12c9da5f1fe3c
M1056063 <M1056063@G1C2ML33283.mindtree.com>
2019-12-20 10:28:11 +0530

    d

--- cafe/src/main/java/com/mindtree/cafe/CafeApplication.java
+++ cafe/src/main/java/com/mindtree/cafe/CafeApplication.java
@@ -3,11 +3,14 @@ package com.mindtree.cafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class CafeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafeApplication.class, args);
+		System.out.print("hi");
 	}
 
 }
